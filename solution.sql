0489ddfab689607db57983b914ae0465932bb709
Faye <fayebheevayne@gmail.com>
2022-03-17 21:12:53 +0800

    added activity code

---
+++ solution.sql
@@ -0,0 +1,11 @@
+-- a. Find all artists that has letter D in its name
+
+-- b. Find all songs that has a length of less than 230
+
+-- c. Join the 'albums' and 'songs' tables. (Only show the album name, song name, and song length.)
+
+-- d. Join the 'artists' and 'albums' tables. (Find all albums that has letter A in its name.)
+
+-- e. Sort the albums in Z-A order. (Show only the first 4 records.)
+
+-- f. Join the 'albums' and 'songs' tables. (Sort albums form Z-A and sort songs from A-Z)
