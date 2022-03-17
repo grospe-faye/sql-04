8f2980b6ec605f96a4c29c06ac832ff3f50723af
Faye <fayebheevayne@gmail.com>
2022-03-17 21:51:23 +0800

    added activity code

--- solution.sql
+++ solution.sql
@@ -1,11 +1,19 @@
 -- a. Find all artists that has letter D in its name
+SELECT * FROM artists WHERE name LIKE "%d%";
 
 -- b. Find all songs that has a length of less than 230
+SELECT * FROM songs WHERE length < 230;
 
 -- c. Join the 'albums' and 'songs' tables. (Only show the album name, song name, and song length.)
+SELECT albums.album_title, songs.song_name, songs.length
+	FROM albums JOIN songs ON albums.id = songs.album_id;
 
 -- d. Join the 'artists' and 'albums' tables. (Find all albums that has letter A in its name.)
 
 -- e. Sort the albums in Z-A order. (Show only the first 4 records.)
+SELECT * FROM albums ORDER BY album_title DESC LIMIT 4;
 
--- f. Join the 'albums' and 'songs' tables. (Sort albums form Z-A and sort songs from A-Z)
+-- f. Join the 'albums' and 'songs' tables. (Sort albums from Z-A and sort songs from A-Z)
+SELECT * FROM albums
+	JOIN songs ON albums.id = songs.album_id
+	ORDER BY album_title DESC AND ORDER BY song_name ASC;	;
